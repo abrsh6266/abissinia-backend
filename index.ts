c7c4331e033b2e6db9800e4f50f2ca826baf10a7
Abrham <abrsh6266@gmail.com>
2024-05-19 11:08:20 -0800

    some updates

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import connectDB from './db';
 import User, { IUser } from './models/User';
+import bcrypt from 'bcryptjs'; 
 
 const app = express();
 const PORT = 4000;
@@ -19,7 +20,7 @@ app.post('/login', async (req: Request, res: Response) => {
     const { username, password } = req.body;
     try {
         const user = await User.findOne({ username });
-        if (user && user.password === password) {
+        if (user && bcrypt.compareSync(password, user.password)) {
             const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
             return res.json({ token });
         }
@@ -30,9 +31,11 @@ app.post('/login', async (req: Request, res: Response) => {
 });
 
 app.post('/register', async (req: Request, res: Response) => {
-    const { username, password,email } = req.body;
+    const { username, password, email } = req.body;
     try {
-        const user = new User({ username, password,email });
+        // Hash the password before saving
+        const hashedPassword = bcrypt.hashSync(password, 10); // Generate a salted hash with 10 rounds
+        const user = new User({ username, password: hashedPassword, email });
         await user.save();
         res.status(201).json({ message: 'User registered' });
     } catch (error) {
