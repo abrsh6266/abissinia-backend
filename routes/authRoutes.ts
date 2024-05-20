5c0950e589db6615909d0d25791d1a717a22f99f
Abrham <abrsh6266@gmail.com>
2024-05-20 04:56:01 -0800

    some updates

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/authController';
+import { login, register, verifyToken } from '../controllers/authController';
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/register', register);
+router.post('/verify-token', verifyToken);
 
 export default router;
+    
